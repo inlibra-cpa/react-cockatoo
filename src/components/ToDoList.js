0d63f834d43d4658ee0c3146a9837d6e13e6982d
inlibra-cpa <inlibra@gmail.com>
2023-02-12 00:32:47 -0500

    added Todo.js to handle functionality of the button

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,9 +1,12 @@
 import React from 'react';
-
+//Import components
+import Todo from './Todo';
 const ToDoList = () => {
     return (
         <div className="todo-container">
-            <ul className="todo-list"></ul>
+            <ul className="todo-list">
+                
+            </ul>
         </div>
     );
 };
