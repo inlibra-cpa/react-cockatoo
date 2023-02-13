6cb621349a689432a5f59468ffa946ad05371489
inlibra-cpa <inlibra@gmail.com>
2023-02-13 01:06:33 -0500

    added functionality to delete btn

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -2,12 +2,18 @@ import React from 'react';
 //Import components
 import Todo from './Todo';
 
-const ToDoList = ({ todos }) => {
+const ToDoList = ({ todos, setTodos }) => {
   return (
     <div className="todo-container">
       <ul className="todo-list">
-        {todos.map(todo => (
-          <Todo key={todo.id} text={todo.text} />
+        {todos.map((todo) => (
+          <Todo 
+          setTodos={setTodos} 
+          todos={todos} 
+          key={todo.id}
+          todo={todo} 
+          text={todo.text} 
+          />
         ))}      
       </ul>
     </div>
